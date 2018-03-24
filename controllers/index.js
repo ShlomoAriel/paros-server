c0aae9da3b1bc6091f4c4f7940873f07048889f0
Shlomo Ariel <shlomoariel@gmail.com>
2018-03-24 15:21:12 +0300

    add require

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,6 @@ var express = require('express')
   , router = express.Router()
 
 router.use('/', require('./contentController'))
+router.use('/', require('./packageController'))
 
 module.exports = router
