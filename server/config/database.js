e2c294074752ceb491485788b9f9709b1e442018
Shlomo Ariel <shlomoariel@gmail.com>
2020-12-05 15:26:47 +0200

    migration

--- server/config/database.js
+++ server/config/database.js
@@ -2,5 +2,6 @@ module.exports = {
   secret: 'devdacticIsAwesome',
   database:
     process.env.DATABASE ||
-    'mongodb://sealion:sealion12345@ds115729.mlab.com:15729/paros',
+    'mongodb+srv://sealion:sealion12345@paros.unfxg.mongodb.net/paros?retryWrites=true&w=majority',
+    // 'mongodb://sealion:sealion12345@ds115729.mlab.com:15729/paros',
 };
